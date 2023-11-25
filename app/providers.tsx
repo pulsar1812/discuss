87c72a52cc89d5671864fc407525ec5e8ed53dc1
pulsar1812 <cluster000@gmail.com>
2023-11-25 17:12:04 +0800

    Initial commit

---
+++ app/providers.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { NextUIProvider } from '@nextui-org/react'
+
+type ProvidersProps = {
+  children: React.ReactNode
+}
+
+export default function Providers({ children }: ProvidersProps) {
+  return <NextUIProvider>{children}</NextUIProvider>
+}
